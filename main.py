937c26ff7bd38a72e8742f475953ce93263878ab
Winnie Cai <79169487+sysadwcai@users.noreply.github.com>
2022-02-13 14:01:15 -0500

    Update main.py
    Main file for the quiz game. Can use Opentdb.com for new questions/answers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+Opentdb.com/api/#questions/category/difficulty/type
 
 question_bank = []
 for question in question_data:
@@ -8,5 +9,12 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
     
-quiz = 
+quiz = QuizBrain(question_bank)
+while still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz.")
+print(F"Your final score is: {score}/{quiz.question_number}.")
+
+      
                              
