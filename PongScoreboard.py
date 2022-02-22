8ac2a1b041bf75a17d791bdefa28482ac103cf28
Winnie Cai <79169487+sysadwcai@users.noreply.github.com>
2022-02-22 14:11:31 -0500

    Update PongScoreboard.py

--- PongScoreboard.py
+++ PongScoreboard.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
 FONT = ('Courier', 80, 'normal')
+WINNING_SCORE = 3
 
 class Scoreboard(Turtle):
     super().__init__()
@@ -16,6 +17,12 @@ class Scoreboard(Turtle):
         self.write(self.l_score, align='center', font=FONT)
         self.goto(100, 200)
         self.write(self.r_score, align='center', font=FONT)
+        if self.l_score == WINNING_SCORE:
+            self.goto(0, 0)
+            self.write('The left Player won!', align='center', font=('Courier', 20, 'normal')
+        elif self.r_score == WINNING_SCORE:
+            self.goto(0, 0)
+            self.write('The right Player won!', align='center', font=('Courier', 20, 'normal')
     
     def l_point(self):
         self.score += 1
