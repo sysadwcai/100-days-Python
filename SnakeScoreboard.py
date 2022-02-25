da81f0e0b4902ebba0355107e5d9671dc612c87d
Winnie Cai <79169487+sysadwcai@users.noreply.github.com>
2022-02-25 15:24:05 -0500

    Update SnakeScoreboard.py
    adding data.txt, reading and writing to the data.txt file to record the high score played

--- SnakeScoreboard.py
+++ SnakeScoreboard.py
@@ -6,7 +6,8 @@ clss Scoreboard(Turtle):
     def __init__(self):
         super().__init__
         self.score = 0
-        self.high_score = 0
+        with open('data.txt','r') as data:
+            self.high_score = int(data.read())
         self.color('white')
         self.penup()
         self.goto(0, 260)
@@ -19,6 +20,8 @@ clss Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open('data.txt', 'w') as data:
+                   data.write(f'{self.high_score}')
         self.score = 0 # reinitialize the score
         self.update_scoreboard()
                    
