2b91dca8a43b041491eabf558d99ad9bab6622fa
Winnie Cai <79169487+sysadwcai@users.noreply.github.com>
2022-03-19 10:18:30 -0400

    updated the unescaping the html code
    using html module translate the escaped characters back to human-readable

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,3 +1,6 @@
+import html
+
+
 class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
@@ -11,7 +14,8 @@ class QuizBrain:
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q: {self.question_number}{current_question.text} (True/False):")
+        q_text = html.unescape(self.current_question.text)
+        user_answer = input(f"Q: {self.question_number}{q_text} (True/False):")
         self.check_answer(user_answer, current_question.answer)
         
     def check_answer(self, user_answer, correct_answer):
