16accee8b9c4699e02c3f7d10c60d7af92afef7d
Winnie Cai <79169487+sysadwcai@users.noreply.github.com>
2022-03-20 08:58:39 -0400

    Update QuizMain.py
    updated with the UI interface

--- QuizMain.py
+++ QuizMain.py
@@ -1,6 +1,7 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from ui import QuizInterface
 
 question_bank = []
 for question in question_data:
@@ -10,8 +11,9 @@ for question in question_data:
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
-while quiz.still_has_questions():
-    quiz.next_question()
+quiz_ui = QuizInterface(quiz)
+# while quiz.still_has_questions():
+#     quiz.next_question()
 
 print("You've completed the quiz.")
 print(f"Your final score is: {quiz.score}/{quiz.question_number}")
