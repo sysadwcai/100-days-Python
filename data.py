dee694060cfe12f15d5c627ed09964990e466c3f
Winnie Cai <79169487+sysadwcai@users.noreply.github.com>
2022-03-20 09:00:31 -0400

    updated data file
    update the data file with interactive api call from opentdb.com

--- data.py
+++ data.py
@@ -9,7 +9,7 @@ response = requests.get(url='https://opentdb.com/api-php', params=parameters)
 response.raise_for_status()
 data = response.json()
 question_data = data['results']
-
+#
 '''question_data = [
         {"category": "Science: Computers",
          "type": "boolean",
