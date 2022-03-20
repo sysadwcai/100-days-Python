d2aa00281368ac84de4dcb613702536bc5eed724
Winnie Cai <79169487+sysadwcai@users.noreply.github.com>
2022-03-20 08:56:22 -0400

    Update quiz_brain.py
    update the next_question to return the value

--- quiz_brain.py
+++ quiz_brain.py
@@ -7,25 +7,29 @@ class QuizBrain:
         self.question_list = q_list
         
     def still_has_questions(self):
-        if self.question_number < len(self.question_list):
-            return True
-        return False
+        return self.question_number < len(self.question_list)
+#         if self.question_number < len(self.question_list):
+#             return True
+#         return False
     
     def next_question(self):
-        current_question = self.question_list[self.question_number]
+        self.current_question = self.question_list[self.question_number]
         self.question_number += 1
         q_text = html.unescape(self.current_question.text)
-        user_answer = input(f"Q: {self.question_number}{q_text} (True/False):")
-        self.check_answer(user_answer, current_question.answer)
+        return f'Q.{self.question_number}: {q_text}'
+#         user_answer = input(f"Q: {self.question_number}{q_text} (True/False):")
+#         self.check_answer(user_answer, current_question.answer)
         
     def check_answer(self, user_answer, correct_answer):
         if user_answer.lower() == correct_answer.lower():
             score += 1
-            print("You got it right!"
+            return True
+            #print("You got it right!"
         else:
-            print("That's wrong.")
-        print(f"The correct answer is: {correct_answer}")
-        print(f"Your current score is: {score}/{self.question_number}.")
-        print('\n')
+            return False
+            #print("That's wrong.")
+#         print(f"The correct answer is: {correct_answer}")
+#         print(f"Your current score is: {score}/{self.question_number}.")
+#         print('\n')
          
             
